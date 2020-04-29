78e147677bd40ce7f9ac5c222deab7b13b72f19a
linshia mbaby <linshiajimmy@linshias-MacBook-Pro.local>
2020-04-29 01:02:34 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UFT-8">
+<meta nmae="viewport" content="width, initial-scale=1.0">
+<title>My First PHP Page</title>
+</head>
+
+<body>
+<h1>My First PHP Page</h1>
+<?php
+echo 'Hello, World';
+?>
+Hello World</body>
+</html>
